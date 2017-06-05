b7b5d610146e06879b63121e6af6ddafbe811252
djedynak259 <dajed@outlook.com>
2017-06-05 14:23:40 -0700

    added clock

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const hi = <p>Test element heress</p>;
+const hi = <p>Test element here - insert text</p>;
 
 class Intro extends Component {
   render() {
@@ -24,6 +24,42 @@ class App extends Component {
         </div>
           <Intro name='Dan'/>
           {hi}
+        <Clock />
+      </div>
+    );
+  }
+}
+
+// Clocl
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
       </div>
     );
   }
