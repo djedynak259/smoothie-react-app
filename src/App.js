ad712d016a11e0fa7a6e6421baf66d5d2d5c53fe
djedynak259 <dajed@outlook.com>
2017-06-07 12:18:14 -0700

    button breakout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Clock from './Clock.js'
+import Clock from './Clock.js';
+import Toggle from './Toggle.js';
 
 const hi = <p>Test element here - insert text</p>;
 
@@ -33,34 +34,6 @@ class App extends Component {
     );
   }
 }
-
-
-// Button
-
-class Toggle extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {isToggleOn: true};
-
-    // This binding is necessary to make `this` work in the callback
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick() {
-    this.setState(prevState => ({
-      isToggleOn: !prevState.isToggleOn
-    }));
-  }
-
-  render() {
-    return (
-      <button onClick={this.handleClick}>
-        {this.state.isToggleOn ? 'ON' : 'OFF'}
-      </button>
-    );
-  }
-}
-
 
 // Greeting
 
