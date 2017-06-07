cc8797f797cb2205b59b86c48c7470f0df010403
djedynak259 <dajed@outlook.com>
2017-06-07 11:38:12 -0700

    greeting added

--- src/App.js
+++ src/App.js
@@ -27,10 +27,13 @@ class App extends Component {
           {hi}
         <Clock />
         <Toggle />
+        <Greeting isLoggedIn={false} />
+        <LoginControl />
       </div>
     );
   }
 }
+
 
 // Button
 
@@ -54,6 +57,85 @@ class Toggle extends React.Component {
       <button onClick={this.handleClick}>
         {this.state.isToggleOn ? 'ON' : 'OFF'}
       </button>
+    );
+  }
+}
+
+
+// Greeting
+
+class UserGreeting extends React.Component {
+  render () {
+    return <h1>Welcome back!</h1>;
+  }
+}
+
+class GuestGreeting extends React.Component {
+  render () {
+    return <h1>Please sign up.</h1>;
+  }
+}  
+
+class Greeting extends React.Component {
+  render()  {
+    const isLoggedIn = this.props.isLoggedIn;
+    if (isLoggedIn) {
+      return <UserGreeting />;
+    }
+    return <GuestGreeting />;
+  }
+}
+
+
+// Login Control
+
+function LoginButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Login
+    </button>
+  );
+}
+
+function LogoutButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Logout
+    </button>
+  );
+}
+
+class LoginControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleLoginClick = this.handleLoginClick.bind(this);
+    this.handleLogoutClick = this.handleLogoutClick.bind(this);
+    this.state = {isLoggedIn: false};
+  }
+
+  handleLoginClick() {
+    this.setState({isLoggedIn: true});
+  }
+
+  handleLogoutClick() {
+    this.setState({isLoggedIn: false});
+  }
+
+  render() {
+    const isLoggedIn = this.state.isLoggedIn;
+
+    let button = null;
+    if (isLoggedIn) {
+      button = <LogoutButton onClick={this.handleLogoutClick} />;
+    } else {
+      button = <LoginButton onClick={this.handleLoginClick} />;
+    }
+
+    return (
+      <div>
+        <Greeting isLoggedIn={isLoggedIn} />
+        {button}
+      </div>
     );
   }
 }
