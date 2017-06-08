b1484bfd2ea46111e511da77de47d633969cc0f8
djedynak259 <dajed@outlook.com>
2017-06-07 17:33:17 -0700

    commit

--- src/NameForm.js
+++ src/NameForm.js
@@ -3,18 +3,18 @@ import React, { Component } from 'react';
 class NameForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: 'enter'};
+    this.state = {textt: 'enter'};
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
-    this.setState({value: event.target.value});
+    this.setState({textt: event.target.value});
   }
 
   handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
+    alert('A name was submitted: ' + this.state.textt);
     event.preventDefault();
   }
 
@@ -23,7 +23,7 @@ class NameForm extends React.Component {
       <form onSubmit={this.handleSubmit}>
         <label>
           Name:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
+          <input type="text" value={this.state.textt} onChange={this.handleChange} />
         </label>
         <input type="submit" value="Submit" />
       </form>
